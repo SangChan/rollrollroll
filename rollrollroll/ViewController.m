ee9d8e631c72b681427633fb1f2f5783b231839c
SangChan Lee <sangchan82@gmail.com>
2015-05-28 17:45:04 +0900

    remove hard coded thing

--- rollrollroll/ViewController.m
+++ rollrollroll/ViewController.m
@@ -33,7 +33,7 @@
     
     _celerbrateLabel.hidden = YES;
     
-     _pickerData = @[@"김순호",@"노윤희",@"이종훈", @"이지은", @"이혜림",@"한재철"];
+     _pickerData = @[];
     
     actual_length = (int)_pickerData.count;
     [[self goButton] defaultStyle];
